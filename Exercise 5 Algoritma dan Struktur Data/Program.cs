ef83ac06e83436b2d9af468e5184b5effccea994
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-12-21 16:13:14 +0700

    create switch case for choosing menu

--- Exercise 5 Algoritma dan Struktur Data/Program.cs
+++ Exercise 5 Algoritma dan Struktur Data/Program.cs
@@ -102,5 +102,52 @@ namespace Exercise_5_Algoritma_dan_Struktur_Data
                 Console.WriteLine();
             }
         }
+        static void Main(string[] args)
+        {
+            Queues q = new Queues();
+            char ch;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1. Implement insert operation");
+                    Console.WriteLine("2. Implement delete operation");
+                    Console.WriteLine("3. Display values");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("\nEnter your choice (1-4): ");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                Console.Write("Enter a number: ");
+                                int num = Convert.ToInt32(System.Console.ReadLine());
+                                Console.WriteLine();
+                                q.insert(num);
+                            }
+                            break;
+                        case '2':
+                            {
+                                q.remove();
+                            }
+                            break ;
+                        case '3':
+                            {
+                                q.display();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid Option!!");
+                            }
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
