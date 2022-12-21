56cd3080288d01a2899c3915d50933970663ac60
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-12-21 15:54:42 +0700

    create remove function

--- Exercise 5 Algoritma dan Struktur Data/Program.cs
+++ Exercise 5 Algoritma dan Struktur Data/Program.cs
@@ -37,5 +37,31 @@ namespace Exercise_5_Algoritma_dan_Struktur_Data
             }
             queue_array[AQIYAS] = element;
         }
+        public void remove()
+        {
+            //Checks queue is empty
+            if (AQIYAS == -1)
+            {
+                Console.WriteLine("Queue underflow\n");
+                return ;
+            }
+            Console.WriteLine("\nThe element deleted from the queue is: " + queue_array[RAYNANDA] + "\n");
+            if (RAYNANDA == AQIYAS)
+            {
+                //Checks queue has one element
+                if (RAYNANDA == AQIYAS)
+                {
+                    RAYNANDA = -1;
+                    AQIYAS = -1;
+                }
+                else
+                {
+                    if (RAYNANDA == max - 1)
+                        RAYNANDA = 0;
+                    else
+                        RAYNANDA = RAYNANDA + 1;
+                }
+            }
+        }
     }
 }
